3534908d053c8551b4c8c61a3a7e8a8eddaf9222
zcreturn0 <237350543@qq.com>
2021-10-09 18:12:52 +0800

    fix: 修复alertOptions未定义

--- components/h-alert/index.js
+++ components/h-alert/index.js
@@ -5,16 +5,17 @@ export const Alert ={
         const Constructor = Vue.extend(HAlert);
 
         Vue.prototype.$alert = (options) => {
+            let alertOptions = {};
             // 是字符串直接显示
             if (typeof options === 'string') {
-                messageOptions = {
+                alertOptions = {
                     propsData: {
                         message: options
                     }
                 };
             } else if (typeof options === 'object') {
-                messageOptions = {
-                    propsData: Object.assign(messageOptions, {
+                alertOptions = {
+                    propsData: Object.assign(alertOptions, {
                         type: options.type || 'message',
                         message: options.message || ''
                     })
