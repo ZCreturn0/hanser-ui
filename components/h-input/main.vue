978edb9b30f68ce4e5cc5972cef3552e1d3af989
zcreturn0 <237350543@qq.com>
2021-10-22 14:43:46 +0800

    feat: 输入框调整

--- components/h-input/main.vue
+++ components/h-input/main.vue
@@ -1,8 +1,9 @@
 <template>
     <!-- 输入框 -->
-    <div class="h-input">
+    <div class="h-input" :size="size">
         <input
             class="h-input__inner"
+            :style="inputStyle"
             :type="type"
             :value="value"
             :placeholder="placeholder"
@@ -11,6 +12,7 @@
 </template>
 
 <script>
+import { INPUT_BG_COLOR, INPUT_TEXT_COLOR, BORDER_COLOR } from '../../theme/var';
 export default {
     name: 'h-input',
     data () {
@@ -28,15 +30,46 @@ export default {
         },
         bgColor: {
             type: String,
-            default: 'transparent'
+            default: INPUT_BG_COLOR
         },
         color: {
             type: String,
-            default: '#333'
+            default: INPUT_TEXT_COLOR
+        },
+        // 边框颜色
+        border: {
+            type: String,
+            default: BORDER_COLOR
         },
         placeholder: {
             type: String,
             default: ''
+        },
+        round: {
+            type: Boolean,
+            default: false
+        },
+        size: {
+            type: String,
+            // ''   'small'   'mini'
+            default: ''
+        }
+    },
+    computed: {
+        inputStyle() {
+            let borderStyle = '';
+            if (this.border === 'none') {
+                borderStyle = '';
+            } else if (!this.border) {
+                borderStyle = `1px solid ${BORDER_COLOR}`;
+            } else {
+                borderStyle = `1px solid ${this.border}`;
+            }
+            return `
+                background-color: ${this.bgColor};
+                color: ${this.color};
+                ${this.borderStyle}
+            `;
         }
     },
     methods: {
